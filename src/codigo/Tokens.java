2b435d0a423dc6c5a5b64120721d35f1f1ffa98a
DELL LATITUDE 5401 <DELL LATITUDE 5401@DESKTOP-N9VRKJJ>
2023-03-31 22:32:12 -0600

    Carga inicial

---
+++ src/codigo/Tokens.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package codigo;
+
+/**
+ *
+ * @author DELL LATITUDE 5401
+ */
+public enum Tokens {
+    Reservadas,
+    Igual,
+    suma,
+    Resta,
+    Multiplicacion,
+    Division,
+    Identificador,
+    Numero,
+    ERROR
+    
+}
